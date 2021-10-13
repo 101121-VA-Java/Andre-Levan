917c1e35941f3d98fd2e4430fe11fb8530bf1410
ALevan747 <92333334+ALevan747@users.noreply.github.com>
2021-10-13 14:07:24 -0700

    Add files via upload

---
+++ colors.java
@@ -0,0 +1,22 @@
+public class colors{
+
+        public static void main(String[] args){
+                int t = 32;
+                System.out.println("Welcome to your Weather");
+                System.out.println("Temperatures (F)");
+                for(t = 32; t < 121; t++){
+                   if(t == 32){
+                      System.out.println("It's 32 degrees and snowing! Bundle u>
+                   }
+                   if (t == 55){
+                      System.out.println("It's 55 degrees and windy. You probab>
+                   }
+                   if (t == 85){
+                      System.out.println("It's 85 degrees and sunny! Put on you>
+                   }
+                   if (t == 120){
+                      System.out.println("It's 120 degrees out! Get inside and >
+                   }
+                }
+        }
+}
